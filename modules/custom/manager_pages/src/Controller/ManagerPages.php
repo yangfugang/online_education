93b394d01c14f167abf7f5e8e21beb66bffb128c
yamus <yang_fugang@163.com>
2017-01-05 17:07:48 +0800

    Temp

---
+++ modules/custom/manager_pages/src/Controller/ManagerPages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\manager_pages\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ManagerPages extends ControllerBase {
+
+    public static function index() {
+        $element = [
+            '#markup' => '<h1>管理员首页</h1>'
+        ];
+
+        return $element;
+    }
+}
