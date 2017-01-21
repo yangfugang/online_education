5651b85796da52e7ee5200703c85fd3aa3b69b97
yamus <yang_fugang@163.com>
2017-01-21 16:52:02 +0800

    Temp

--- modules/custom/manager_pages/src/Controller/ManagerPages.php
+++ modules/custom/manager_pages/src/Controller/ManagerPages.php
@@ -6,7 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 
 class ManagerPages extends ControllerBase {
 
-    public static function index() {
+    public function index() {
         $element = [
             '#markup' => '<h1>管理员首页</h1>'
         ];
--- sites/default/settings.php
+++ sites/default/settings.php
@@ -715,15 +715,15 @@ $settings['trusted_host_patterns'] = array(
  *
  * Keep this code block at the end of this file to take full effect.
  */
-# if (file_exists(__DIR__ . '/settings.local.php')) {
-#   include __DIR__ . '/settings.local.php';
-# }
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
 $databases['default']['default'] = array (
   'database' => 'd8',
   'username' => 'root',
-  'password' => 'yl',
+  'password' => 'yl19900808!',
   'prefix' => '',
-  'host' => 'db',
+  'host' => '127.0.0.1',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
